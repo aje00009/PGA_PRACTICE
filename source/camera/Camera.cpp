7c423b5b539c526faf4c5556584353f80f0cada7
Alberto Jiménez Expósito <aje00009@red.ujaen.es>
2025-10-11 16:43:16 +0200

    Creación de clase Camera

---
+++ source/camera/Camera.cpp
@@ -0,0 +1,8 @@
+//
+// Created by alber on 09/10/2025.
+//
+
+#include "Camera.h"
+
+namespace PAG {
+} // PAG
---
+++ source/camera/Camera.h
@@ -0,0 +1,13 @@
+//
+// Created by alber on 09/10/2025.
+//
+
+#ifndef PGA_PRACTICA_CAMERA_H
+#define PGA_PRACTICA_CAMERA_H
+
+namespace PAG {
+    class Camera {
+    };
+} // PAG
+
+#endif //PGA_PRACTICA_CAMERA_H
